cfd9570d388f4403e07ffad883ad955823a08b53
Connor Breen <conchors@gmail.com>
2025-08-30 13:50:06 -0400

    testing if i can push

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -5,7 +5,8 @@ public class Board : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //testing to see if i can push
+        Debug.Log("meow");
     }
 
     // Update is called once per frame
